16bdca736544b8bd92d984704982f109f21b4d74
Your Name <you@example.com>
2015-02-12 21:36:13 +0000

    works

--- m.py
+++ m.py
@@ -1,7 +1,11 @@
 import pibrella as p
 import time
 import sys
+import os
+from pprint import pprint
 
+from data import morse
+pprint(morse)
 def render_morse(s):
 	print(s)
 
@@ -11,29 +15,45 @@ def press(pin):
 class Button(object):
 	def __init__(self):
 		self.pressed = False
-		self.last_pressed_at = None
+		self.last_pressed_at = time.time()
+		self.last_upped_at = time.time()
 		p.button.changed(self.press)
+		self.message = ''
+		self.all = ''
 
 	def press(self, pin):
-		if self.pressed:
+		if not pin.read():
+			self.last_upped_at = time.time()
 			duration = time.time() - self.last_pressed_at
 			self.up(duration)
-			self.last_pressed_at = None
-		else:
-			self.down()
 			self.last_pressed_at = time.time()
-		self.pressed = pin.read()
+		else:
+			self.last_pressed_at = time.time()
+			duration_up = time.time() - self.last_upped_at
+			self.down(duration_up)
 
-	def down(self):
+	def down(self, duration_up):
+		if duration_up > 0.7:
+			if self.message in morse or len(self.message)>4:
+				os.system('clear')
+				letter = morse[self.message]
+				self.all += letter
+				print(self.all)
+			
+			self.message = ''
 		p.buzzer.note(3)
 
 	def up(self, duration):
 		p.buzzer.off()
-		if duration > .3:
-			print('-', end='')
+		if duration > .1:
+			letter = '-'
 		else:
-			print('.', end='')
+			letter = '.'
+		self.message += letter
+		print(letter, end='')
 		sys.stdout.flush()
+		
+		
 
 	
 if __name__ == '__main__':
